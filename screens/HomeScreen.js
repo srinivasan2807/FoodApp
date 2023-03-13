c8ed5d1ae524cbca084f9df33e0847a45c827068
srini <fuzzysugarboy@gmail.com>
2023-03-13 17:31:00 +0530

    First

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-700">HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
